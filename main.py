1a0b2066b27c7761546ac20785a6fe9168e4bb87
BrunoBernardinelli82 <brunobernardinelli@gmail.com>
2024-12-06 17:44:35 -0300

    commit inicial IA

---
+++ main.py
@@ -0,0 +1,11 @@
+# pip install langchain
+# pip install langchain-community
+# pip install langchain-huggingface
+# pip install streamlit
+# pip install python-dotenv
+# pip install torch==2.3.1 torchvision torchaudio
+# pip install -q transformers einops accelerate bitsandbytes
+# pip install langchain-ollama
+# pip install langchain-openai
+
+import streamlit as st
