cec3bf10f4a836bb2bcd4471f29b0e4e3b70465a
BrunoBernardinelli82 <brunobernardinelli@gmail.com>
2024-12-06 18:11:11 -0300

    1 criando a logica  do main

--- main.py
+++ main.py
@@ -9,3 +9,24 @@
 # pip install langchain-openai
 
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import MessagesPlaceholder
+
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import torch
+from langchain_huggingface import ChatHuggingFace
+from langchain_community.llms import huggingface_hub
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="Seu Assistente Virtual ", page_icon="")
+st.title("Seu Assistente Virtual")
+st.button("Botao")
+st.chat_input("Digite sua mensagem")
